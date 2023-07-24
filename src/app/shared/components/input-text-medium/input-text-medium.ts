2df013b3427990939775d8b4cb85070bcd0f9eb2
Productos-Union <manager@union.pe>
2023-07-24 13:32:03 -0500

    medium

--- src/app/shared/components/input-text-medium/input-text-medium.ts
+++ src/app/shared/components/input-text-medium/input-text-medium.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'input-text-medium',
   templateUrl: './input-text-medium.html',
 })
-export class InputTextMedium {}
+export class InputTextMedium {
+
+  @Input() borderColor: string = '';
+  @Input() fondoColor: string = '';
+  @Input() text: string = '';
+}
