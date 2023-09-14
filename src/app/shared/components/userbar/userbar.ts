e2b8f83d714efdcbab3e3de8153f2a28ca142ca3
mariollori <mariollori@upeu.edu.pe>
2023-09-14 18:55:33 -0500

    userbar

--- src/app/shared/components/userbar/userbar.ts
+++ src/app/shared/components/userbar/userbar.ts
@@ -1,39 +1,49 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CircleAvatarComponent } from '../circle-avatar/circle-avatar.component';
 import { CommonModule, NgClass } from '@angular/common';
 
 
 
 @Component({
-  standalone:true,
-  selector : 'userbar-ppp',
+  standalone: true,
+  selector: 'userbar-ppp',
   templateUrl: './userbar.html',
-  styleUrls:['./userbar.css'],
-  imports:[CircleAvatarComponent, NgClass,CommonModule]
+  styleUrls: ['./userbar.css'],
+  imports: [CircleAvatarComponent, NgClass, CommonModule]
 
 
 })
 
-export class UserBar{
-   
-       list = [
-        {icon:'home-2',title:'Home'},
-        {icon:'setting-1',title:'Admin'},
-        {icon:'clock',title:'Options'},
-        {icon:'home-2',title:'Home'},
-        {icon:'setting-1',title:'Admin'},
-        {icon:'clock',title:'Options'},
-        {icon:'home-2',title:'Home'},
-        {icon:'setting-1',title:'Admin'},
-        {icon:'clock',title:'Options'},
-        {icon:'home-2',title:'Home'},
-        {icon:'setting-1',title:'Admin'},
-        {icon:'clock',title:'Options'},
-        
-      
-       ]
-       userbarVisible: boolean = true;
-       toogleSidebar() {
-          this.userbarVisible = !this.userbarVisible;
-        }
+export class UserBar implements OnInit {
+
+  validateRol:string='';
+
+  roles:any[] = []
+
+  ngOnInit(): void {
+    this.roles = JSON.parse(sessionStorage.getItem('user')!).roles.map((value:any)=> {this.validateRol+='/'+value.name})
+  }
+
+
+
+  list = [
+    { icon: 'home-2', title: 'Home' },
+    { icon: 'setting-1', title: 'Admin' },
+    { icon: 'clock', title: 'Options' },
+    { icon: 'home-2', title: 'Home' },
+    { icon: 'setting-1', title: 'Admin' },
+    { icon: 'clock', title: 'Options' },
+    { icon: 'home-2', title: 'Home' },
+    { icon: 'setting-1', title: 'Admin' },
+    { icon: 'clock', title: 'Options' },
+    { icon: 'home-2', title: 'Home' },
+    { icon: 'setting-1', title: 'Admin' },
+    { icon: 'clock', title: 'Options' },
+
+
+  ]
+  userbarVisible: boolean = true;
+  toogleSidebar() {
+    this.userbarVisible = !this.userbarVisible;
+  }
 }
