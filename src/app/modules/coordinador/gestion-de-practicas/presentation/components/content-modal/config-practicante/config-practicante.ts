5b3a35b761b6b9e82f59bccd7852315f8b0acaf0
MadaiMireli <madaichuco@hotmail.com>
2023-09-29 11:00:41 -0500

    Modal UseCase 06

--- src/app/modules/coordinador/gestion-de-practicas/presentation/components/content-modal/config-practicante/config-practicante.ts
+++ src/app/modules/coordinador/gestion-de-practicas/presentation/components/content-modal/config-practicante/config-practicante.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { InputTextMedium } from "src/app/shared/components/input-text-medium/input-text-medium";
+import { GlobalModel } from "src/app/shared/components/modal/global-modal";
 
 @Component({
   standalone: true,
@@ -7,9 +9,24 @@ import { InputTextMedium } from "src/app/shared/components/input-text-medium/inp
   templateUrl: './config-practicante.html',
   imports: [
     InputTextMedium,
+    GlobalModel,
+    CommonModule
   ]
 })
 
 export class ConfigPracticanteModal {
 
+  isShowModal = false
+  documentCvCharged: File | undefined = undefined
+
+  onUploadDocument(event: Event) {
+
+    const elementFile = event.target as HTMLInputElement;
+
+    this.isShowModal = true
+    this.documentCvCharged = elementFile.files![0];
+
+    console.log("entro a abrir el modalsito")
+
+  }
 }
